ef6c747bc66f9a049833f7ff73c165694ec07b45
Williancosta98 <willian.c0sta4167@gmail.com>
2024-02-02 11:31:19 -0300

    subindo o projeto

---
+++ products.js
@@ -0,0 +1,8 @@
+const menuOptions = [
+    { name: '<b>X</b> - Salada', price: 30, vegan: false, src: './img/xsalada.jpeg' },
+    { name: '<b>X</b> - Bacon', price: 34, vegan: false, src: './img/xbacon.png' },
+    { name: '<b>X</b> - Bacon Egg', price: 39, vegan: false, src: './img/bacon-egg.png' },
+    { name: '<b>M</b>onstruoso', price: 50, vegan: false, src: './img/monstruoso.png' },
+    { name: '<b>X</b> - Vegan', price: 45, vegan: true, src: './img/xvegan.png' },
+    { name: '<b>Big</b> Vegano', price: 55, vegan: true, src: './img/monstruoso-vegan.png' },
+    ]
